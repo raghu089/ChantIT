fb5787b134eef0f20edd7873856dd16b5d33a017
raghavendraShet <91419832+raghu089@users.noreply.github.com>
2023-08-14 10:15:16 +0530

    JMHome screen added

---
+++ Controller/JapaMandala/JMHomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  JMHomeViewController.swift
+//  ChantIT
+//
+//  Created by Bitjini Apps and More on 13/08/23.
+//
+
+import UIKit
+
+class JMHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+   
+
+}
